113685f89ff45200316051dacb159e01e317aa36
creatior <vartem2020@mail.ru>
2025-03-18 22:06:33 +0300

    LR1.3 area and volume

--- Program.fs
+++ Program.fs
@@ -1,4 +1,6 @@
-﻿type SolveResult =
+﻿open System
+
+type SolveResult =
   | None
   | Linear of float
   | Quadratic of float * float
@@ -12,6 +14,18 @@ let solve a b c =
         if D<0. then None
         else Quadratic(((-b+sqrt(D))/2.*a),(-b-sqrt(D)) / (2.*a))
 
+let circleArea radius = 
+    let area = Math.PI * radius * radius
+    area
+
+let cylinderVolume radius height =
+    let area = circleArea radius
+    area * height
+
+let multipleAreaHeight area h = area * h
+
+let cylinderVolumeSuperPos = (circleArea >> multipleAreaHeight)
+
 [<EntryPoint>]
 let main args = 
     //printfn "Hello, world!"
@@ -19,10 +33,23 @@ let main args =
     let result = solve 4. 2. -1.
 
     match result with 
-        None -> printf "Корней нет"
-        | Linear(x) -> printf "Линейное уравнение, корень: %f" x
-        | Quadratic(x1, x2) when x1=x2 -> printf "Квадратное уравнение, 1 корень: %f" x1
-        | Quadratic(x1, x2) -> printf "Квадратное уравнение, 2 корня: %f %f" x1 x2
+        None -> printf "Корней нет\n"
+        | Linear(x) -> printf "Линейное уравнение, корень: %f\n" x
+        | Quadratic(x1, x2) when x1=x2 -> printf "Квадратное уравнение, 1 корень: %f\n" x1
+        | Quadratic(x1, x2) -> printf "Квадратное уравнение, 2 корня: %f %f\n" x1 x2
+
+    System.Console.Write("Введите радиус: ")
+    let r = System.Console.ReadLine() |> float
+    System.Console.Write("Введите высоту: ")
+    let h = System.Console.ReadLine() |> float
+
+    let volume = cylinderVolume r h
+
+    System.Console.WriteLine("Объем (каррирование): " + volume.ToString())
+
+    let volume2 = cylinderVolumeSuperPos r h
+
+    System.Console.WriteLine("Объем: (суперпозиция): " + volume2.ToString())
 
     System.Console.ReadKey() |> ignore
     0
