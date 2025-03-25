cfea0975b482810d391b9ffe9a4a48a56d2edb5a
creatior <vartem2020@mail.ru>
2025-03-25 22:29:36 +0300

    LR1.20 processing tasks

--- Program.fs
+++ Program.fs
@@ -1,6 +1,5 @@
 ﻿open System
 open HelloWorld
-open QuadraticEquation
 open SumDigits
 open Fibonacci
 open CylinderVolume
@@ -14,93 +13,22 @@ let task6 bool =
         | true -> sumDigits
         | false -> fibonacciUp
 
+let task20 num arg =
+    match num with
+        | 1 -> countEvenNotCoprime arg
+        | 2 -> maxDigitNotDiv3 arg
+        | 3 -> computeProduct arg
+        | _ -> 
+            Console.WriteLine("Неверный номер задачи")
+            0
 
 
 [<EntryPoint>]
-let main args =     
-    printHelloWorld
-
-    let result = solve 4. 2. -1.
-
-    match result with 
-        None -> printf "Корней нет\n\n"
-        | Linear(x) -> printf "Линейное уравнение, корень: x = %f\n\n" x
-        | Quadratic(x1, x2) when x1=x2 -> printf "Квадратное уравнение, 1 корень: x = %f\n\n" x1
-        | Quadratic(x1, x2) -> printf "Квадратное уравнение, 2 корня: x1 = %f x2 = %f\n\n" x1 x2
-
-    System.Console.Write("Введите радиус: ")
-    //let r = System.Console.ReadLine() |> float
-    System.Console.Write("Введите высоту: ")
-    //let h = System.Console.ReadLine() |> float
-
-    //let volume = cylinderVolume r h
-
-    //System.Console.WriteLine("Объем (каррирование): " + volume.ToString())
-
-    //let volume2 = cylinderVolumeSuperPos r h
-
-    //System.Console.WriteLine("Объем: (суперпозиция): " + volume2.ToString() + "\n")
-
-    //System.Console.Write("Сумма цифр\nВведите число: ")
-    //let n = System.Console.ReadLine() |> int
-
-    //let sum1 = sumDigitsUp n
-    //System.Console.WriteLine("Сумма цифр (рекурсия вверх): " + sum1.ToString())
-
-    //let sum2 = sumDigits n
-    //System.Console.WriteLine("Сумма цифр (рекурсия вниз): " + sum2.ToString())
-
-    let result1 = fibonacciUp 19
-    System.Console.WriteLine(result1.ToString())
-    let result2 = fibonacciUp 19
-    System.Console.WriteLine(result2.ToString())
-    let resultTask6 = task6 false 14
-    System.Console.WriteLine(resultTask6.ToString())
-
-    let num = 756766
-    let result7 = traverse num (fun a b -> (a + b))
-    System.Console.WriteLine(result7.ToString())
-
-    let result8 = traverse num (fun a b -> max a b)
-    System.Console.WriteLine(result8.ToString())
-
-    let result9 = traverseWithPredicate num (fun a b -> (a + b)) (fun a -> (a % 2 = 1))
-    System.Console.WriteLine(result9.ToString())
-
-    let result10 = traverseWithPredicate num (fun a b -> max a b) (fun a -> (a % 2 = 1))
-    System.Console.WriteLine(result10.ToString())
-    
-    //let language = System.Console.ReadLine() |> string
-    //favouriteProgrammingLanguage language
-
-    //favLangSuperPos ()
-    //favLangCarry ()
-
-    let number = 25
-
-    let result11 = coprimeTraversal number (+) 0
-    System.Console.WriteLine(result11.ToString())
-
-    let result12 = coprimeTraversal number (fun a b -> a + 1) 0
-    System.Console.WriteLine(result12.ToString())
-
-    let result13 = coprimeTraversal number (fun a b -> max a b) 0
-    System.Console.WriteLine(result13.ToString())
-
-    let result14 = coprimeTraversal number (fun a b -> min a b) number
-    System.Console.WriteLine(result14.ToString())
-
-    let result15 = eulerFunction number
-    System.Console.WriteLine(result15.ToString())
-
-    let result16 = countEvenNotCoprime number
-    Console.WriteLine(result16)
-
-    let result17 = maxDigitNotDiv3 number
-    Console.WriteLine(result17)
-
-    let result18 = computeProduct number
-    Console.WriteLine(result18)
+let main args =
+    Console.WriteLine("Введите номер задачи и аргумент: ")
+    let task = Console.ReadLine() |> int
+    let arg = Console.ReadLine() |> int
+    Console.WriteLine(task20 task arg)
 
     System.Console.ReadKey() |> ignore
     0
--- obj/Debug/net6.0/FuncLab1.AssemblyInfo.fs
+++ obj/Debug/net6.0/FuncLab1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("FuncLab1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d144296806273f9353ebf90d883b83bf9737fdc")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c82b7e7af95c0ec369002f0b990c4d16445ce5c")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FuncLab1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FuncLab1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
